762eeeea2ed409c013024013a5bd51f63b34da11
sena <hargiantofebrisena@gmail.com>
2022-05-03 19:56:20 +0700

    initial

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,15 +1,45 @@
 const { user, profile, chat } = require("../../models");
 
 const socketIo = (io) => {
+  io.use((socket, next) => {
+    if (socket.handshake.auth && socket.handshake.auth.token) {
+      next();
+    } else {
+      next(new Error("Not Authorized"));
+    }
+  });
+
   io.on("connection", (socket) => {
     console.log("client connect:", socket.id);
-
     socket.on("load admin contact", async () => {
       try {
         const adminContact = await user.findOne({
           where: {
             status: "admin",
           },
+          include: [
+            {
+              model: profile,
+              as: "profiles",
+              attributes: {
+                exclude: ["createdAt", "updatedAt"],
+              },
+            },
+            {
+              model: chat,
+              as: "recipientMessage",
+              attributes: {
+                exclude: ["createdAt", "updatedAt"],
+              },
+            },
+            {
+              model: chat,
+              as: "senderMessage",
+              attributes: {
+                exclude: ["createdAt", "updatedAt"],
+              },
+            },
+          ],
           attributes: {
             exclude: ["createdAt", "updatedAt", "password"],
           },
@@ -50,14 +80,6 @@ const socketIo = (io) => {
             exclude: ["createdAt", "updatedAt", "password"],
           },
         });
-
-        // custommerContacts = JSON.parse(JSON.stringify(custommerContacts));
-
-        // custommerContacts = custommerContacts.map((item) => {
-        //   return {
-        //     ...item,
-        //   };
-        // });
 
         socket.emit("custommer contacts", custommerContacts);
       } catch (err) {
