e1ded7dbc742f7b667bebaeb8a7f7ee196137550
sena <hargiantofebrisena@gmail.com>
2022-05-09 11:59:54 +0700

    initial

--- src/controller/transaction.js
+++ src/controller/transaction.js
@@ -176,8 +176,8 @@ exports.notification = async (req, res) => {
         updateTransaction("pending", orderId);
         res.status(200);
       } else if (fraudStatus == "accept") {
+        sendEmail("success", orderId);
         updateTransaction("success", orderId);
-        // sendEmail("success", orderId);
         res.status(200);
       }
     } else if (transactionStatus == "settlement") {
@@ -215,13 +215,13 @@ const updateTransaction = async (status, transactionId) => {
 
 const sendEmail = async (status, transactionId) => {
   console.log(status);
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.SYSTEM_EMAIL,
-      pass: process.env.SYSTEM_PASSWORD,
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   service: "gmail",
+  //   auth: {
+  //     user: process.env.SYSTEM_EMAIL,
+  //     pass: process.env.SYSTEM_PASSWORD,
+  //   },
+  // });
 
   let data = await transaction.findOne({
     where: {
@@ -250,44 +250,46 @@ const sendEmail = async (status, transactionId) => {
 
   data = JSON.parse(JSON.stringify(data));
 
-  const mailOptions = {
-    from: process.env.SYSTEM_EMAIL,
-    to: data.buyer.email,
-    subject: "Payment status",
-    text: "Your payment is <br />" + status,
-    html: `<!DOCTYPE html>
-            <html lang="en">
-              <head>
-                <meta charset="UTF-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Document</title>
-                <style>
-                  h1 {
-                    color: brown;
-                  }
-                </style>
-              </head>
-              <body>
-                <h2>Product payment :</h2>
-                <ul style="list-style-type:none;">
-                  <li>Name : ${data.product.name}</li>
-                  <li>Total payment: ${convertRupiah.convert(data.price)}</li>
-                  <li>Status : <b>${status}</b></li>
-                </ul>  
-              </body>
-            </html>`,
-  };
+  console.log(data);
 
-  if (data.status != status) {
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) throw err;
-      console.log("Email sent: " + info.response);
+  // const mailOptions = {
+  //   from: process.env.SYSTEM_EMAIL,
+  //   to: data.buyer.email,
+  //   subject: "Payment status",
+  //   text: "Your payment is <br />" + status,
+  //   html: `<!DOCTYPE html>
+  //           <html lang="en">
+  //             <head>
+  //               <meta charset="UTF-8" />
+  //               <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  //               <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  //               <title>Document</title>
+  //               <style>
+  //                 h1 {
+  //                   color: brown;
+  //                 }
+  //               </style>
+  //             </head>
+  //             <body>
+  //               <h2>Product payment :</h2>
+  //               <ul style="list-style-type:none;">
+  //                 <li>Name : ${data.product.name}</li>
+  //                 <li>Total payment: ${convertRupiah.convert(data.price)}</li>
+  //                 <li>Status : <b>${status}</b></li>
+  //               </ul>
+  //             </body>
+  //           </html>`,
+  // };
 
-      return res.send({
-        status: "Success",
-        message: info.response,
-      });
-    });
-  }
+  // if (data.status != status) {
+  //   transporter.sendMail(mailOptions, (err, info) => {
+  //     if (err) throw err;
+  //     console.log("Email sent: " + info.response);
+
+  //     return res.send({
+  //       status: "Success",
+  //       message: info.response,
+  //     });
+  //   });
+  // }
 };
