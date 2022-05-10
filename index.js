14d44cf48af6787b4b66e3a0791f8aadc27ea243
sena <hargiantofebrisena@gmail.com>
2022-05-10 18:58:00 +0700

    initial

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // we must define cors because our client and server have diffe
+    origin: "https://www.diafragma.xyz/", // we must define cors because our client and server have diffe
   },
 });
 
